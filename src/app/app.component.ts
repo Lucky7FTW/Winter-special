e125d1584191667618c13c982ead164a204de648
Lucky7 <mihaitibrea2@gmail.com>
2024-12-20 12:47:33 +0200

    versiune cu keyframes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule]
 })
-export class AppComponent {
-  title = 'winter-special';
+export class AppComponent implements OnInit {
+  snowflakes: { left: number; delay: number; duration: number; size: number }[] = [];
+
+  ngOnInit() {
+    const numberOfFlakes = 50;
+    for (let i = 0; i < numberOfFlakes; i++) {
+      this.snowflakes.push({
+        left: Math.random() * 100,
+        delay: Math.random() * 5,
+        duration: 8 + Math.random() * 5,
+        size: 1 + Math.random() * 1.5
+      });
+    }
+  }
 }
